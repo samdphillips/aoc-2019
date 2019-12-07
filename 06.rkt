3bc8ca0cfdbf123a8392f28bedaa0faf97435d22
Sam Phillips <samdphillips@gmail.com>
2019-12-07 15:18:48 -0800

    Day 6 - part 2

--- 06.rkt
+++ 06.rkt
@@ -1,7 +1,11 @@
 #lang racket/base
 
-(require racket/port
+(require racket/match
+         racket/port
+         racket/set
+         racket/stream
          racket/string
+         graph
          memoize
          threading)
 
@@ -9,6 +13,30 @@
   (require rackunit))
 
 (struct orbit (center satelite) #:transparent)
+
+(struct orbital-map (orbits)
+  #:transparent
+  #:methods gen:graph
+  [(define (in-vertices an-orbital-map)
+     (for/fold ([v (seteq)])
+               ([an-orbit (in-orbits an-orbital-map)])
+       (set-add (set-add v (orbit-center an-orbit))
+                (orbit-satelite an-orbit))))
+
+   (define (in-neighbors an-orbital-map body)
+     (define (iter orbits)
+       (match orbits
+         ['() empty-stream]
+         [(cons (orbit (== body) satelite) rest)
+          (stream-cons satelite (iter rest))]
+         [(cons (orbit center (== body)) rest)
+          (stream-cons center (iter rest))]
+         [(cons _ rest) (iter rest)]))
+     (iter (in-orbits an-orbital-map)))
+   ])
+
+(define (in-orbits an-orbital-map)
+  (orbital-map-orbits an-orbital-map))
 
 (define (read-orbit in)
   (define line (read-line in))
@@ -19,8 +47,8 @@
           (map string->symbol)
           (apply orbit))]))
 
-(define (read-all-orbits in)
-  (port->list read-orbit in))
+(define (read-orbital-map in)
+  (orbital-map (port->list read-orbit in)))
 
 (module+ test
   (check-equal?
@@ -28,14 +56,15 @@
    (orbit 'A 'B))
 
   (check-equal?
-   (call-with-input-string "A12)B34\nA12)C56" read-all-orbits)
-   (list (orbit 'A12 'B34)
-         (orbit 'A12 'C56))))
+   (call-with-input-string "A12)B34\nA12)C56" read-orbital-map)
+   (orbital-map
+    (list (orbit 'A12 'B34)
+          (orbit 'A12 'C56)))))
 
 
 (define (orbit-count-checksum list-of-orbits)
   (define links
-    (for/hash ([an-orbit (in-list list-of-orbits)])
+    (for/hash ([an-orbit (in-orbits list-of-orbits)])
       (values (orbit-satelite an-orbit)
               (orbit-center an-orbit))))
 
@@ -64,12 +93,44 @@ INPUT
     )
   (check-equal?
    (orbit-count-checksum
-    (call-with-input-string test-input-map read-all-orbits))
+    (call-with-input-string test-input-map read-orbital-map))
    42))
 
 (module* part-one #f
   (orbit-count-checksum
-   (call-with-input-file "inputs/06.txt" read-all-orbits)))
+   (call-with-input-file "inputs/06.txt" read-orbital-map)))
+
+(define (transfers-between an-orbital-map src dest)
+  (define-values (step-table path-table)
+    (dijkstra an-orbital-map src))
+  (- (hash-ref step-table dest) 2))
+
+(module+ test
+  (define test-input-transfers #<<INPUT
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN
+INPUT
+    )
+  (let ([orbital-map (call-with-input-string test-input-transfers
+                                             read-orbital-map)])
+    (check-equal? (transfers-between orbital-map 'YOU 'SAN)
+                  4)))
 
 
-(module* part-two #f)
+(module* part-two #f
+  (transfers-between (call-with-input-file "inputs/06.txt"
+                       read-orbital-map)
+                     'YOU
+                     'SAN))
+
